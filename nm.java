83cba2443dd9241ac9096f47aa1ed1526e093c35
Arya Dev <35726067+Captainpanda25@users.noreply.github.com>
2020-09-26 19:23:09 +0530

    Number Mirror

---
+++ nm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nm {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		System.out.println(n);
+
+	}
+
+}
