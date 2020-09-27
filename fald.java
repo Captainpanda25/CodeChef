7b29f193b2f0d73db20fcad63e93f52dd9ff20a5
Arya Dev <35726067+Captainpanda25@users.noreply.github.com>
2020-09-27 16:17:33 +0530

    First and Last digit
    c-02

--- fald.java
+++ fald.java
@@ -3,32 +3,16 @@ import java.util.Scanner;
 public class fald {
 
 	public static void main(String[] args) {
-		int t,n,c,m,s;
+		int t,n,s;
 		Scanner in =new Scanner(System.in);
 		t=in.nextInt();
 		while(t-->0)
 		{
-			c=0;
-			s=0;
 			n=in.nextInt();
-			m=n;							//Storing value of the integer in m
-			if(n/10<1)
-			{
-				s=n;						//if n is a single digit number, simply return itself
-			}
-			else {
-			while(n>0)
-			{
-				c=n%10;						//Storing each digit till we come out of loop, hence retains the last value stored(first digit)
-				if(n==m)					//taking the last digit
-				{
-					s=c;
-				}
-				n=n/10;
-			}
-			s+=c;							//Adding the first and last digits
-			}
-			System.out.println(s);			//printing the sum
+			s=0;
+			char[] ch = ("" + n).toCharArray();
+			s=Integer.parseInt(String.valueOf(ch[0]))+Integer.parseInt(String.valueOf(ch[ch.length-1]));
+			System.out.println(s);
 		}
 
 	}
